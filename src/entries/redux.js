68647a3cd8e5ae93e5db3dc6470c7a3051acf2e9
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-01-12 20:09:03 -0600

    initial redux data

---
+++ src/entries/redux.js
@@ -0,0 +1,9 @@
+const form = document.getElementById('form')
+form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+  event.preventDefault()
+  const data = new FormData(form)
+  const title = data.get('title')
+  console.log(title);
+}
