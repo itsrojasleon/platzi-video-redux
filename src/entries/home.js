1d6815f416eb36d16a0567b5d1029354d1ab863e
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-01-16 16:00:06 -0600

    adding a store

--- src/entries/home.js
+++ src/entries/home.js
@@ -4,10 +4,20 @@ import Home from '../pages/containers/home';
 // import Playlist from './src/playlist/components/playlist';
 import data from '../api.json';
 // console.log('Hola mundo!' )
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+const initialState = { data }
+
+const store = createStore(
+  (state) => state,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+console.log(store.getState());
 
 const homeContainer = document.getElementById('home-container')
 
 // ReactDOM.render(que voy a renderizar, donde lo haré);
 // const holaMundo = <h1>hola Estudiante!</h1>;
 hydrate( <Home data={data} />, homeContainer);
-
