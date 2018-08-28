f08b11519df3cbcba1d21a846b289ab1c0c6efd6
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-08-28 14:00:32 -0500

    SPA in Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -15,10 +15,8 @@ module.exports = (env) => {
   }
 
   return {
-
     entry: {
-      "home": path.resolve(__dirname, 'src/entries/home.js'),
-      "redux": path.resolve(__dirname, 'src/entries/redux.js'),
+      "app": path.resolve(__dirname, 'src/entries/app.js')
     },
     output: {
       path: path.resolve(__dirname, 'dist'),
